1c357e6940e32363c5d227674906110b03428c7b
Soteded <43409872+Soteded@users.noreply.github.com>
2020-11-02 19:47:31 +0100

    LocationTry

--- TP5_GPSApp/src/pages/Live.tsx
+++ TP5_GPSApp/src/pages/Live.tsx
@@ -1,7 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import './custom.css'
+
+import { save } from 'ionicons/icons';
+
+import LocationComponent from '../components/GeolocationComponent';
 
 const Live: React.FC = () => {
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,7 +16,19 @@ const Live: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-          <IonTitle></IonTitle>
+        <IonGrid className="ion-margin">
+          <IonRow>
+            <IonCol>
+              <IonTitle>Your current position is :</IonTitle>
+            </IonCol>
+          </IonRow>
+          <LocationComponent />
+          <IonRow>
+            <IonCol>
+              <IonButton fill='outline'><IonIcon slot="start" icon={save} /> <IonLabel slot='end'>Save!</IonLabel></IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
--- TP5_GPSApp/src/pages/Profile.tsx
+++ TP5_GPSApp/src/pages/Profile.tsx
@@ -1,6 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import './common.css';
+import './custom.css'
 
 const Profile: React.FC = () => {
   return (
@@ -11,7 +11,20 @@ const Profile: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-          <IonTitle></IonTitle>
+        <IonList>
+          <IonItem id='name'>
+            <IonLabel>Nom</IonLabel>
+            <IonLabel className="ion-text-end">Yann</IonLabel>
+          </IonItem>
+          <IonItem id='lastLat'>
+            <IonLabel>Dernière Latitude :</IonLabel>
+            <IonLabel className="ion-text-end">10</IonLabel>
+          </IonItem>
+          <IonItem id='lastLon'>
+            <IonLabel>Dernière Longitude :</IonLabel>
+            <IonLabel className="ion-text-end">-0.5</IonLabel>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
