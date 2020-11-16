5ff203d368f680fd3cbb8d9e2ae90fbf44f74f57
FlorianLeveil <florian.leveil@ynov.com>
2020-11-16 16:47:11 +0100

    add modal searchadduser with request to the bdd

--- PROJECT/src/components/ContactAddUserModal.tsx
+++ PROJECT/src/components/ContactAddUserModal.tsx
@@ -1,25 +1,21 @@
 import { IonAvatar, IonButton, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonListHeader, IonModal, IonSearchbar, IonTitle, IonToolbar } from '@ionic/react';
 import React, { useContext, useEffect, useState } from 'react';
 import firebase from '../firebase';
-
-import AppContext from '../data/app-context';
+import AppContext, { UserData } from '../data/app-context';
 import { text } from 'ionicons/icons';
 
-const ContactAddUserModal: React.FC<{ showModal: boolean, setShowModal: (value: boolean) => void }> = (props) => {
+const ContactAddUserModal: React.FC<{ showModal: boolean,setShowModal: (value: boolean) => void }> = (props) => {
+    const [searchData, setSearchData] = useState<firebase.firestore.DocumentData[]>([])
     const appCtx = useContext(AppContext);
     const [searchText, setSearchText] = useState('');
-    const contacts = ['user1', 'user2', 'user3', 'user4', 'user5'];
 
     useEffect(() => {
         if (searchText.length >= 3) {
-            firebase.firestore().collection('Users').where('email', '>', searchText).get()
+            firebase.firestore().collection('Users').orderBy('email').startAt(searchText).endAt(searchText + "\uf8ff").get()
                 .then((res) => {
-                    res.docs.forEach((res2) => {
-                        console.log(res2.data().email);
-                    });
+                    setSearchData(res.docs)
                 })
         }
-        console.log('-----------')
     })
 
     return (
@@ -34,19 +30,25 @@ const ContactAddUserModal: React.FC<{ showModal: boolean, setShowModal: (value: 
             </IonHeader>
 
             <IonContent fullscreen>
-            <IonSearchbar autocorrect="off" onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
+                <IonSearchbar autocorrect="off" onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
                 <IonList>
-                    <IonItem>
-                        <IonAvatar slot="start">
-                            <img alt='Profile' src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
-
-                        </IonAvatar>
-                        <IonLabel>
-                            <h2>{searchText}</h2>
-                            <p>fgaapld@coco.com</p>
-                        </IonLabel>
-                        <IonButton color="success" size="default">Ajouter</IonButton>
-                    </IonItem>
+                {
+                    searchData.map( (value, key) => {
+                        return (
+                    
+                            <IonItem key={key}>
+                                <IonAvatar slot="start">
+                                    <img alt='Profile' src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
+                                </IonAvatar>
+                                <IonLabel>
+                                    <h2>{value.data()?.username}</h2>
+                                    <p>{value.data()?.email}</p>
+                                </IonLabel>
+                                <IonButton color="success" size="default">Ajouter</IonButton>
+                            </IonItem>
+                        )
+                    })
+                }
                 </IonList>
             </IonContent>
         </IonModal>
