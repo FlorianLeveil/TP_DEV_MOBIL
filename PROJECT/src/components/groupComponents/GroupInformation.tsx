688cacf16cd9ad9b443efbfc531629e8d708da28
Soteded <43409872+Soteded@users.noreply.github.com>
2020-12-07 00:21:49 +0100

    Update GroupInformation.tsx

--- PROJECT/src/components/groupComponents/GroupInformation.tsx
+++ PROJECT/src/components/groupComponents/GroupInformation.tsx
@@ -1,5 +1,5 @@
-import { IonAlert, IonItem, IonLabel, IonList, IonModal } from '@ionic/react';
-import React, { useContext, useState } from 'react';
+import { IonButton, IonContent, IonFooter, IonHeader, IonItem, IonLabel, IonModal, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useContext, useEffect, useState } from 'react';
 import AppContext, { Group } from '../../data/app-context';
 import firebase from "../../firebase";
 
@@ -7,10 +7,26 @@ interface oui {
     [key: string]: firebase.firestore.DocumentData;
 }
 
-const GroupInformationItem: React.FC<{action: string, text: string, users: oui, group: Group}> = (props) => {
+const GroupInformationItem: React.FC<{action: string, text: string, group: Group}> = (props) => {
     const [showModal, setShowModal] = useState<boolean>(false);
     const appCtx = useContext(AppContext);
     const [usrList, setUsrList] = useState<string[]>([]);
+    const [contactList, setContactList] = useState<firebase.firestore.DocumentData[]>([]);
+
+    useEffect(() => {
+        setContactList([]);
+        appCtx.contacts.contactList.forEach( async (user) => {
+            await firebase.firestore().collection("Users").doc(user).get()
+                .then((res) => {
+                    setContactList((prevState) => {
+                        let newList = [...prevState];
+                        newList.push(res);
+                        return newList;
+                    })
+                })
+        });
+    // eslint-disable-next-line
+    }, [appCtx.contacts.contactList])
 
     const whatToDisplay = () => {
         switch (props.action) {
@@ -18,117 +34,117 @@ const GroupInformationItem: React.FC<{action: string, text: string, users: oui, 
                 return (
                     <IonModal
                         isOpen={showModal}
-                    />
+                        onDidDismiss={() => {setShowModal(false)}}
+                    >
+                        <IonHeader translucent>
+                            <IonToolbar>
+                                    <IonTitle>Add User</IonTitle>
+                                    <IonButton slot="end" type="reset" fill="clear" onClick={() => {
+                                        setShowModal(false)
+                                    }}>Retour</IonButton>
+                            </IonToolbar>
+                        </IonHeader>
+                        <IonContent>
+                            
+                        </IonContent>
+                        <IonFooter>
+
+                        </IonFooter>
+                    </IonModal>
                 )
             case "remUser":
                 return (
-                    <IonAlert
-                        isOpen={showAlert}
-                        onDidDismiss={() => setShowAlert(false)}
-                        message={`oui`}
-                        buttons={[
-                            {
-                                text: 'Cancel',
-                                role: 'cancel',
-                                cssClass: 'secondary',
-                                handler: () => {
-                                    console.log('Confirm Cancel');
-                                }
-                            },
-                            {
-                                text: 'Ok',
-                                cssClass: 'primary',
-                                handler: (data: any) => {
-                                    console.log("Clicked !");
-                                    setShowAlert(false);
-                                }
-                            }
-                        ]}
-                    />
+                    <IonModal
+                        isOpen={showModal}
+                        onDidDismiss={() => {setShowModal(false)}}
+                    >
+                        <IonHeader translucent>
+                            <IonToolbar>
+                                    <IonTitle>Remove User</IonTitle>
+                                    <IonButton slot="end" type="reset" fill="clear" onClick={() => {
+                                        setShowModal(false)
+                                    }}>Retour</IonButton>
+                            </IonToolbar>
+                        </IonHeader>
+                        <IonContent>
+
+                        </IonContent>
+                        <IonFooter>
+                            
+                        </IonFooter>
+                    </IonModal>
                 )
             case "eleUser":
                 return (
-                    <IonAlert
-                        isOpen={showAlert}
-                        onDidDismiss={() => setShowAlert(false)}
-                        message={`oui`}
-                        buttons={[
-                            {
-                                text: 'Cancel',
-                                role: 'cancel',
-                                cssClass: 'secondary',
-                                handler: () => {
-                                    console.log('Confirm Cancel');
-                                }
-                            },
-                            {
-                                text: 'Ok',
-                                cssClass: 'primary',
-                                handler: (data: any) => {
-                                    console.log("Clicked !");
-                                    setShowAlert(false);
-                                }
-                            }
-                        ]}
-                    />
+                    <IonModal
+                        isOpen={showModal}
+                        onDidDismiss={() => {setShowModal(false)}}
+                    >
+                        <IonHeader translucent>
+                            <IonToolbar>
+                                    <IonTitle>Elevate User</IonTitle>
+                                    <IonButton slot="end" type="reset" fill="clear" onClick={() => {
+                                        setShowModal(false)
+                                    }}>Retour</IonButton>
+                            </IonToolbar>
+                        </IonHeader>
+                        <IonContent>
+
+                        </IonContent>
+                        <IonFooter>
+                            
+                        </IonFooter>
+                    </IonModal>
                 )
             case "retUser":
                 return (
-                    <IonAlert
-                        isOpen={showAlert}
-                        onDidDismiss={() => setShowAlert(false)}
-                        message={`oui`}
-                        buttons={[
-                            {
-                                text: 'Cancel',
-                                role: 'cancel',
-                                cssClass: 'secondary',
-                                handler: () => {
-                                    console.log('Confirm Cancel');
-                                }
-                            },
-                            {
-                                text: 'Ok',
-                                cssClass: 'primary',
-                                handler: (data: any) => {
-                                    console.log("Clicked !");
-                                    setShowAlert(false);
-                                }
-                            }
-                        ]}
-                    />
+                    <IonModal
+                        isOpen={showModal}
+                        onDidDismiss={() => {setShowModal(false)}}
+                    >
+                        <IonHeader translucent>
+                            <IonToolbar>
+                                    <IonTitle>Retrograde User</IonTitle>
+                                    <IonButton slot="end" type="reset" fill="clear" onClick={() => {
+                                        setShowModal(false)
+                                    }}>Retour</IonButton>
+                            </IonToolbar>
+                        </IonHeader>
+                        <IonContent>
+
+                        </IonContent>
+                        <IonFooter>
+                            
+                        </IonFooter>
+                    </IonModal>
                 )
             case "delConv":
                 return (
-                    <IonAlert
-                        isOpen={showAlert}
-                        onDidDismiss={() => setShowAlert(false)}
-                        message={`oui`}
-                        buttons={[
-                            {
-                                text: 'Cancel',
-                                role: 'cancel',
-                                cssClass: 'secondary',
-                                handler: () => {
-                                    console.log('Confirm Cancel');
-                                }
-                            },
-                            {
-                                text: 'Ok',
-                                cssClass: 'primary',
-                                handler: (data: any) => {
-                                    console.log("Clicked !");
-                                    setShowAlert(false);
-                                }
-                            }
-                        ]}
-                    />
+                    <IonModal
+                        isOpen={showModal}
+                        onDidDismiss={() => {setShowModal(false)}}
+                    >
+                        <IonHeader translucent>
+                            <IonToolbar>
+                                    <IonTitle>Delete groupe</IonTitle>
+                                    <IonButton slot="end" type="reset" fill="clear" onClick={() => {
+                                        setShowModal(false)
+                                    }}>Retour</IonButton>
+                            </IonToolbar>
+                        </IonHeader>
+                        <IonContent>
+
+                        </IonContent>
+                        <IonFooter>
+                            
+                        </IonFooter>
+                    </IonModal>
                 )
         }
     }
 
     return (
-        <IonItem onClick={() => {setShowAlert(true)}}>
+        <IonItem onClick={() => {setShowModal(true)}}>
             <IonLabel>
                 {props.text}
             </IonLabel>
